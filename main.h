3af2bba234bb8b65a0343e28bce1948eacda894a
elcodo254 <ellawachaga@gmail.com>
2022-12-23 17:19:10 -0800

    include structs in header file

--- main.h
+++ main.h
@@ -8,6 +8,26 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+/**
+ * aliases - struct
+ * @a_name: alias name
+ * @name: real name
+ */
+typedef struct aliases
+{
+	char *a_name;
+	char *name;
+} alias;
+
+/**
+ * builts - struct for builtins
+ * @name: pointer to name
+ */
+typedef struct builts
+{
+	char *name;
+	int (*func)(void);
+} builts;
 
 int execute(char **argv);
 char *get_path(char *cmd);
